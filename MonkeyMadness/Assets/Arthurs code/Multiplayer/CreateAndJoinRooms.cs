6ca46408721ceb76c3a2763141201c0f0ee84366
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2022-11-17 13:35:00 +0100

    multiplayer klaar om te testen

--- MonkeyMadness/Assets/Arthurs code/Multiplayer/CreateAndJoinRooms.cs
+++ MonkeyMadness/Assets/Arthurs code/Multiplayer/CreateAndJoinRooms.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 
 public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 {
-
+    [Header("Input fields", order = 0)]
     public InputField createInput;
     public InputField joinInput;
-
+    [Header("Room Options", order = 1)]
+    public float maxPlayers;
     public void CreateRoom() {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = (byte)maxPlayers;
+        Debug.Log("Creating room with name: " + createInput.text);
         PhotonNetwork.CreateRoom(createInput.text);
     }
 
     public void JoinRoom() {
+        Debug.Log("Joining room with name: " + joinInput.text);
         PhotonNetwork.JoinRoom(joinInput.text);
     }
 
