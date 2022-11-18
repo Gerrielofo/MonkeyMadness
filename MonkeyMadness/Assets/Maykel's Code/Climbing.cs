f02d85d5a882423b17c07cb237424aab91eb1ecd
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2022-11-18 15:28:01 +0100

    Climbing XR rig

--- MonkeyMadness/Assets/Maykel's Code/Climbing.cs
+++ MonkeyMadness/Assets/Maykel's Code/Climbing.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 public class Climbing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- MonkeyMadness/Assets/Maykel's Code/ClimbingInteractable.cs
+++ MonkeyMadness/Assets/Maykel's Code/ClimbingInteractable.cs
@@ -4,17 +4,5 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 public class ClimbingInteractable : XRBaseInteractable
 {
-    protected override void OnSelectEntered(XRBaseInteractor interactor)
-    {
-        base.OnSelectEntered(interactor);
 
-        Debug.Log("SELECT ENTER");
-    }
-
-    protected override void OnSelectExited(XRBaseInteractor interactor)
-    {
-        base.OnSelectExited(interactor);
-
-        Debug.Log("SELECT EXIT");
-    }
 }
