73809d6b49e6070fedc9b69e75f7cb6f1fa8c2fe
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2022-11-21 15:40:47 +0100

    fix

--- MonkeyMadness/Assets/PlayerControlManager.cs
+++ MonkeyMadness/Assets/PlayerControlManager.cs
@@ -8,11 +8,18 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class PlayerControlManager : MonoBehaviourPunCallbacks {
     public ActionBasedController leftController;
     public ActionBasedController rightController;
+    public Component[] componentsToDissable;
     public PhotonView photonView;
     // Start is called before the first frame update
     void Start()
     {
         if (!photonView.IsMine) {
+            MonoBehaviour[] comps = GetComponents<MonoBehaviour>();
+            foreach (MonoBehaviour c in comps) {
+                if (c != photonView) {
+                    c.enabled = false;
+                }
+            }
             leftController.enabled = false;
             rightController.enabled = false;
         }
