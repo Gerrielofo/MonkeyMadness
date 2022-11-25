df78e515dcc5cf4bf435b4443664af411ec3699c
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2022-11-25 15:28:25 +0100

    multiplayer syncen bijna compleet gefixed
    alleen animaties nog fixen

--- MonkeyMadness/Assets/Arthurs code/Multiplayer/SpawnPlayers.cs
+++ MonkeyMadness/Assets/Arthurs code/Multiplayer/SpawnPlayers.cs
@@ -7,6 +7,7 @@ using Photon.Realtime;
 public class SpawnPlayers : MonoBehaviourPunCallbacks
 {
     public GameObject spawnedPlayerPrefab;
+    public GameObject ghostPrefab;
     public float minX, maxX, minY, maxY;
     private string playerName;
     private int playerNumber;
@@ -18,7 +19,7 @@ public class SpawnPlayers : MonoBehaviourPunCallbacks
         Debug.Log("Joined room");
         Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
         Vector2 fixedspawn = new Vector2(10, 2);
-        spawnedPlayerPrefab = PhotonNetwork.Instantiate("XROriginCloseGrabbibng", fixedspawn, Quaternion.identity);
+        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", fixedspawn, Quaternion.identity);
         Debug.Log(spawnedPlayerPrefab.name);
         Debug.Log(PhotonNetwork.LocalPlayer.NickName);
     }
