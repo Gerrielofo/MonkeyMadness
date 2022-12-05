d05cd121c88d6bae939c9dbc89c3ee949c18f762
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2022-12-05 16:34:02 +0100

    fix

--- MonkeyMadness/Assets/Gerlof's code/Dartbord/DartbordSection.cs
+++ MonkeyMadness/Assets/Gerlof's code/Dartbord/DartbordSection.cs
@@ -6,12 +6,12 @@ public class DartbordSection : MonoBehaviour
 {
     [SerializeField] private int MinigameID;
     [SerializeField] private string MinigameName;
-    private OnDoor Door;
+    
     private MinigameLoader MinigameLoader;
 
     private void Start()
     {
-        Door = GameObject.Find("Door").GetComponent<OnDoor>();
+        
         MinigameLoader = GameObject.Find("MinigameHitbox").GetComponent<MinigameLoader>();
     }
     private void OnCollisionEnter(Collision collision)
@@ -22,32 +22,26 @@ public class DartbordSection : MonoBehaviour
             {
                 case 1:
                     print("Playing " + MinigameName);
-                    Door.openDoor.SetTrigger("OpenDoor");
                     MinigameLoader.MinigameName = MinigameName;
                     break;
                 case 2:
                     print("Playing " + MinigameName);
-                    Door.openDoor.SetTrigger("OpenDoor");
                     MinigameLoader.MinigameName = MinigameName;
                     break;
                 case 3:
                     print("Playing " + MinigameName);
-                    Door.openDoor.SetTrigger("OpenDoor");
                     MinigameLoader.MinigameName = MinigameName;
                     break;
                 case 4:
                     print("Playing " + MinigameName);
-                    Door.openDoor.SetTrigger("OpenDoor");
                     MinigameLoader.MinigameName = MinigameName;
                     break;
                 case 5:
                     print("Playing " + MinigameName);
-                    Door.openDoor.SetTrigger("OpenDoor");
                     MinigameLoader.MinigameName = MinigameName;
                     break;
                 case 6:
                     print("Playing " + MinigameName);
-                    Door.openDoor.SetTrigger("OpenDoor");
                     MinigameLoader.MinigameName = MinigameName;
                     break;
             }
--- MonkeyMadness/Assets/Gerlof's code/Dartbord/MinigameLoader.cs
+++ MonkeyMadness/Assets/Gerlof's code/Dartbord/MinigameLoader.cs
@@ -5,11 +5,18 @@ using Photon.Pun;
 public class MinigameLoader : MonoBehaviour
 {
     public string MinigameName;
+    private OnDoor Door;
+
+    private void Start()
+    {
+        Door = GameObject.Find("Door").GetComponent<OnDoor>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (MinigameName != string.Empty && other.transform.tag == "Hands") 
         {
             print("Loading minigame:" + MinigameName);
+            Door.openDoor.SetTrigger("OpenDoor");
             PhotonNetwork.LoadLevel(MinigameName);
         }
     }
