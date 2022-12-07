c32f0d9754de01ad6d1666c43f661a683240b6c3
Gerrielofo <gerlof.reinink@gmail.com>
2022-12-07 11:41:21 +0100

    Stun with stars and delay

--- MonkeyMadness/Assets/Gerlof's code/Spleef/StunPlayer.cs
+++ MonkeyMadness/Assets/Gerlof's code/Spleef/StunPlayer.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class StunPlayer : MonoBehaviour
 {
+    [SerializeField] private Vector3 starsOffset;
+    [SerializeField] private GameObject starsEffect;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.CompareTag("IsPlayer"))
         {
             collision.transform.GetComponent<ExpandedMovementProvider>().Stun();
+            GameObject stars = (GameObject)Instantiate(starsEffect, collision.transform.position + starsOffset, collision.transform.rotation * Quaternion.Euler(-90, 0, 0));
             Destroy(gameObject);
+            Destroy(stars, collision.transform.GetComponent<ExpandedMovementProvider>().stunTime);
         }
     }
 }
