2bdc320033d4cc442fc6ffba6e88d130e2cf9f1c
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2022-12-13 16:13:47 +0100

    player id test

--- MonkeyMadness/Assets/Arthurs code/Minigames/BowlenManager.cs
+++ MonkeyMadness/Assets/Arthurs code/Minigames/BowlenManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class BowlenManager : MonoBehaviour
-{
+public class BowlenManager : MonoBehaviourPunCallbacks {
+    public GameObject pinBundel;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,9 @@ public class BowlenManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer) {
+        Debug.Log(newPlayer.UserId);
+        base.OnPlayerEnteredRoom(newPlayer);
     }
 }
