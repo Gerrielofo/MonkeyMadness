a69aad408b13c13f7b9057a1c0d4a37bff202184
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2022-12-13 17:03:16 +0100

    bezig met player id

--- MonkeyMadness/Assets/Arthurs code/Minigames/BowlenManager.cs
+++ MonkeyMadness/Assets/Arthurs code/Minigames/BowlenManager.cs
@@ -6,10 +6,15 @@ using Photon.Realtime;
 
 public class BowlenManager : MonoBehaviourPunCallbacks {
     public GameObject pinBundel;
+    public Transform[] pinLocations;
+    public Player[] playerList;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerList = PhotonNetwork.PlayerList;
+        foreach (Player player in PhotonNetwork.PlayerList) {
+            Debug.Log(player.NickName + ", User ID =" + player.UserId);
+        }
     }
 
     // Update is called once per frame
@@ -17,8 +22,5 @@ public class BowlenManager : MonoBehaviourPunCallbacks {
     {
         
     }
-    public override void OnPlayerEnteredRoom(Player newPlayer) {
-        Debug.Log(newPlayer.UserId);
-        base.OnPlayerEnteredRoom(newPlayer);
-    }
+
 }
