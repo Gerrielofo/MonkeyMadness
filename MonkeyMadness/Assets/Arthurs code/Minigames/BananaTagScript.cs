a577d81386ad0d683209d235b9526bcd3bad643b
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2022-12-16 13:23:01 +0100

    usegravity banaan uitgezet

--- MonkeyMadness/Assets/Arthurs code/Minigames/BananaTagScript.cs
+++ MonkeyMadness/Assets/Arthurs code/Minigames/BananaTagScript.cs
@@ -6,7 +6,8 @@ public class BananaTagScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Player") {
-            Transform bananholder = other.gameObject.transform.GetChild(0).GetChild(1);
+            Transform bananholder = other.gameObject.transform.GetChild(0).GetChild(2).GetChild(1);
+            transform.GetComponent<Rigidbody>().useGravity = false;
             transform.position = bananholder.position;
             transform.parent = bananholder;
         }
