469dc942a3ae9e72aaabec64c85e5913ccd6c2e1
Gerrielofo <gerlof.reinink@gmail.com>
2022-12-16 15:42:12 +0100

    Sounds settings

---
+++ MonkeyMadness/Assets/Gerlof's code/UXUI/SoundManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        }
+
+        else
+        {
+            load();
+        }
+
+
+    }
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        save();
+    }
+
+    private void load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+}
