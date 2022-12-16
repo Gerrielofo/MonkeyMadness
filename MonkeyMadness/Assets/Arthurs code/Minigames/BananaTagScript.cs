cf3ff94041c3d2780958515e696d060e9f8a6274
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2022-12-16 13:51:20 +0100

    object syncing test

--- MonkeyMadness/Assets/Arthurs code/Minigames/BananaTagScript.cs
+++ MonkeyMadness/Assets/Arthurs code/Minigames/BananaTagScript.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Photon.Pun;
 
 public class BananaTagScript : MonoBehaviour
 {
+    private PhotonView photonView;
+    // Start is called before the first frame update
+    void Start() {
+        photonView = GetComponent<PhotonView>();
+    }
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Player") {
+            photonView.RequestOwnership();
             Transform bananholder = other.gameObject.transform.GetChild(0).GetChild(2).GetChild(1);
             transform.GetComponent<Rigidbody>().useGravity = false;
             transform.GetComponent<BoxCollider>().isTrigger = true;
