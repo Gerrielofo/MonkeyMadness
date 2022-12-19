e12b3088399e77e1d59c19f14a9f6196df47118a
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2022-12-19 12:40:05 +0100

    Update BananaTagScript.cs

--- MonkeyMadness/Assets/Arthurs code/Minigames/BananaTagScript.cs
+++ MonkeyMadness/Assets/Arthurs code/Minigames/BananaTagScript.cs
@@ -7,28 +7,29 @@ using Photon.Pun;
 public class BananaTagScript : MonoBehaviour {
     private PhotonView photonView;
     private bool cooldown;
+    public Collider previouseHolder;
     // Start is called before the first frame update
     void Start() {
         photonView = GetComponent<PhotonView>();
     }
     private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("IsPlayer") && !cooldown) {
+        if (other.CompareTag("IsPlayer") && !cooldown && other != previouseHolder) {
+            previouseHolder = other;
+            cooldown = true;
+            photonView.RequestOwnership();
+            Transform bananholder = other.transform.parent.GetChild(2).GetChild(1);
+            print("multiplayerRig");
+            transform.position = bananholder.position;
+            transform.parent = null;
+            transform.position = bananholder.position;
+            transform.parent = bananholder;
+            transform.GetComponent<Rigidbody>().useGravity = false;
+            transform.GetComponent<BoxCollider>().isTrigger = true;
+            transform.position = bananholder.position;
             StartCoroutine(GiveBanan(other));
-
         }
     }
     IEnumerator GiveBanan(Collider other) {
-        cooldown = true;
-        photonView.RequestOwnership();
-        Transform bananholder = other.transform.parent.GetChild(2).GetChild(1);
-        print("multiplayerRig");
-        transform.position = bananholder.position;
-        transform.parent = null;
-        transform.position = bananholder.position;
-        transform.parent = bananholder;
-        transform.GetComponent<Rigidbody>().useGravity = false;
-        transform.GetComponent<BoxCollider>().isTrigger = true;
-        transform.position = bananholder.position;
         yield return new WaitForSeconds(5);
         cooldown = false;
     }
