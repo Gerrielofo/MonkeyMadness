cbf638a2227f4303aee831da9dc5e066469e95fb
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2022-12-19 16:17:35 +0100

    yeye

--- MonkeyMadness/Assets/Arthurs code/Minigames/BananaTagScript.cs
+++ MonkeyMadness/Assets/Arthurs code/Minigames/BananaTagScript.cs
@@ -8,29 +8,34 @@ public class BananaTagScript : MonoBehaviour {
     private PhotonView photonView;
     private bool cooldown;
     public Collider previouseHolder;
+    public GameObject[] players;
     // Start is called before the first frame update
     void Start() {
         photonView = GetComponent<PhotonView>();
+        
+        players = GameObject.FindGameObjectsWithTag("IsPlayer");
+        Collider other = players[Random.Range(0, PhotonNetwork.PlayerList.Length)].GetComponent<Collider>();
+        GiveBanan(other);
     }
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("IsPlayer") && !cooldown && other != previouseHolder) {
-            previouseHolder = other;
-            cooldown = true;
-            photonView.RequestOwnership();
-            Transform bananholder = other.transform.parent.GetChild(2).GetChild(1);
-            print("multiplayerRig");
-            transform.position = bananholder.position;
-            transform.parent = null;
-            transform.position = bananholder.position;
-            transform.parent = bananholder;
-            transform.GetComponent<Rigidbody>().isKinematic = true;
-            transform.GetComponent<Rigidbody>().useGravity = false;
-            transform.GetComponent<BoxCollider>().isTrigger = true;
-            transform.position = bananholder.position;
+
             StartCoroutine(GiveBanan(other));
         }
     }
     IEnumerator GiveBanan(Collider other) {
+        previouseHolder = other;
+        cooldown = true;
+        photonView.RequestOwnership();
+        Transform bananholder = other.transform.parent.GetChild(2).GetChild(1);
+        transform.position = bananholder.position;
+        transform.parent = null;
+        transform.position = bananholder.position;
+        transform.parent = bananholder;
+        transform.GetComponent<Rigidbody>().isKinematic = true;
+        transform.GetComponent<Rigidbody>().useGravity = false;
+        transform.GetComponent<BoxCollider>().isTrigger = true;
+        transform.position = bananholder.position;
         yield return new WaitForSeconds(5);
         cooldown = false;
     }
