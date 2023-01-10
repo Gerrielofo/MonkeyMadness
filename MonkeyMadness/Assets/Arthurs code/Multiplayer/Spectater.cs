95093b7a613e8b8c2f2fe08ea1cf55eeafffb992
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2023-01-10 16:44:25 +0100

    help

---
+++ MonkeyMadness/Assets/Arthurs code/Multiplayer/Spectater.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Spectater : MonoBehaviour
+{
+    public Transform cagelocation;
+    public void Start() {
+
+    }
+
+}
