9c2177ddc8f1b57e9f1371bd070b55fd7893f6c7
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2023-01-11 15:44:28 +0100

    puntensysteem is af

--- MonkeyMadness/Assets/Maykel's Code/Dart/PointSystem.cs
+++ MonkeyMadness/Assets/Maykel's Code/Dart/PointSystem.cs
@@ -17,20 +17,38 @@ public class PointSystem : MonoBehaviour
     public int[] players;
     private int pm;
 
-    public List<int> tList = new List<int>() { 12, 40, 3, 10 };
-    public bool kaas;
+    public PhotonView photonView;
 
+    public List<int> tList;
+    public bool kaas;
+    public IDictionary<int, int> playerPoints;
     ExitGames.Client.Photon.Hashtable playerInfo = new ExitGames.Client.Photon.Hashtable();
-    private void Start()
+    public void Start()
     {
         // OPSLAAN
-
-        Debug.Log(Int32.Parse(PhotonNetwork.LocalPlayer.UserId));
-
+        //Debug.Log(Int32.Parse(PhotonNetwork.LocalPlayer.UserId));
+        IDictionary<int, int> playerPoint = new Dictionary<int, int>();
+        playerPoint.Add(1, 12);
+        playerPoint.Add(2, 40);
+        playerPoint.Add(3, 3);
+        playerPoint.Add(4, 10);
+        playerPoints = playerPoint;
+        Placement();
+        /*
         playerInfo["pointsP" + PhotonNetwork.LocalPlayer.UserId] = points[Int32.Parse(PhotonNetwork.LocalPlayer.UserId)];
         playerInfo["monkeyP" + PhotonNetwork.LocalPlayer.UserId] = monkey[Int32.Parse(PhotonNetwork.LocalPlayer.UserId)];
         playerInfo["placementP" + PhotonNetwork.LocalPlayer.UserId] = placement[Int32.Parse(PhotonNetwork.LocalPlayer.UserId)];
         playerInfo["minigameP" + PhotonNetwork.LocalPlayer.UserId] = minigamePoints[Int32.Parse(PhotonNetwork.LocalPlayer.UserId)];
+        */
+    }
+    public void AddPoints(int points, int userId) {
+        playerPoints[userId] = points;
+    }
+    public void Placement(){
+        var ordered = playerPoints.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        foreach (KeyValuePair<int, int> kvp in ordered.Reverse()) {
+            Debug.Log(kvp.Key.ToString() + kvp.Value.ToString());
+        }
     }
     public void Update()
     {
