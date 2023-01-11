6b2b0be51d3e38c1cef43d9b133124444cfd7eb1
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-01-11 13:15:30 +0100

    push

--- MonkeyMadness/Assets/Maykel's Code/Dart/PointSystem.cs
+++ MonkeyMadness/Assets/Maykel's Code/Dart/PointSystem.cs
@@ -11,22 +11,52 @@ public class PointSystem : MonoBehaviour
     public GameObject[] monkey;
     public int[] placement;
 
-    public int[] minigamePoints;
+    public List<int> minigamePoints;
     public int firstplace;
+
     public int[] players;
+    private int pm;
+
+    public List<int> tList = new List<int>() { 12, 40, 3, 10 };
+    public bool kaas;
+
     ExitGames.Client.Photon.Hashtable playerInfo = new ExitGames.Client.Photon.Hashtable();
     private void Start()
     {
         // OPSLAAN
-        
+
+        Debug.Log(Int32.Parse(PhotonNetwork.LocalPlayer.UserId));
+
         playerInfo["pointsP" + PhotonNetwork.LocalPlayer.UserId] = points[Int32.Parse(PhotonNetwork.LocalPlayer.UserId)];
         playerInfo["monkeyP" + PhotonNetwork.LocalPlayer.UserId] = monkey[Int32.Parse(PhotonNetwork.LocalPlayer.UserId)];
         playerInfo["placementP" + PhotonNetwork.LocalPlayer.UserId] = placement[Int32.Parse(PhotonNetwork.LocalPlayer.UserId)];
         playerInfo["minigameP" + PhotonNetwork.LocalPlayer.UserId] = minigamePoints[Int32.Parse(PhotonNetwork.LocalPlayer.UserId)];
     }
-    private void Update()
+    public void Update()
     {
         // vergelijk player id met player die eerste die geworden
+
+        //uncomment na testen
+        //var tList = new List<int>();
+        //tList = minigamePoints.ToList();
+
+
+
+        //for (int i = 0; i < tList.ToArray().Length; i++)
+        //{
+        //    if(tList.ToArray()[i] == tList.ToArray().Max())
+        //    {
+        //        placement[pm] = tList.ToArray()[i];
+        //        pm++;
+        //        tList.Remove(i);
+        //        i = 0;
+        //    }
+        //}
+        //if (!kaas)
+        //{
+        //    Debug.Log(placement.ToString());
+        //    kaas = true;
+        //}
     }
     public void CalculateBananaTag()
     {
@@ -36,15 +66,13 @@ public class PointSystem : MonoBehaviour
     }
     public void CalculateBowling()
     {
-        Array.Reverse(minigamePoints);
         playerInfo["minigameP" + PhotonNetwork.LocalPlayer.UserId] = placement[Int32.Parse(PhotonNetwork.LocalPlayer.UserId)];
     }
     public void CalculatePlacement()
     {
-        Array.Sort(minigamePoints);
     }
     public void CalculatePoints()
     {
-        playerInfo["pointsP" + PhotonNetwork.LocalPlayer.UserId] =+ placement[Int32.Parse(PhotonNetwork.LocalPlayer.UserId)];
+        playerInfo["pointsP" + PhotonNetwork.LocalPlayer.UserId] = +placement[Int32.Parse(PhotonNetwork.LocalPlayer.UserId)];
     }
 }
