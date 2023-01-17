aceaadcb26df21e0843f12cfbe028227ff9bca3c
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-01-17 15:15:52 +0100

    Update DartbordSection.cs

--- MonkeyMadness/Assets/Gerlof's code/Dartbord/DartbordSection.cs
+++ MonkeyMadness/Assets/Gerlof's code/Dartbord/DartbordSection.cs
@@ -12,7 +12,6 @@ public class DartbordSection : MonoBehaviour
     {
         if (collision.transform.CompareTag("Dart")){
             ReadyRoomSystem.minigameToLoad = MinigameName;
-
         }
     }
 }
