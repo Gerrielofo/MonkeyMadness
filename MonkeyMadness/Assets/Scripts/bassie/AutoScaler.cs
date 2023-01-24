f2df128f9f94b27bdfa2f9517f0a1cbc49f30ede
Gamim3 <99258180+Gamim3@users.noreply.github.com>
2023-01-24 17:04:44 +0100

    hight + canvas

--- MonkeyMadness/Assets/Scripts/bassie/AutoScaler.cs
+++ MonkeyMadness/Assets/Scripts/bassie/AutoScaler.cs
@@ -9,22 +9,34 @@ public class AutoScaler : MonoBehaviour
     public GameObject cameraHeight;
     public GameObject player;
 
+    float distance;
+
     private void Start()
+    {
+        StartCoroutine(WaitAndResize());
+    }
+
+    public void ButtonResize()
     {
         StartCoroutine(WaitAndResize());
     }
 
     void Resize()
     {
+        RaycastHit hit;
+        if (Physics.Raycast(cameraHeight.transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+        {
+            distance = hit.distance;
+        }
         float headHeight = cameraHeight.transform.localPosition.y;
-        float scale = MonkeyHeight / headHeight;
+        float scale = distance / headHeight * MonkeyHeight;
         transform.localScale = Vector3.one * scale;
     }
 
 
     public IEnumerator WaitAndResize()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(2);
         Resize();
     }
 }
