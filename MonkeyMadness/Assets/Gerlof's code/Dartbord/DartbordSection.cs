6cd8a36127ab12d588ff013e8525f6eb6679fa56
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2023-01-24 14:22:25 +0100

    test

--- MonkeyMadness/Assets/Gerlof's code/Dartbord/DartbordSection.cs
+++ MonkeyMadness/Assets/Gerlof's code/Dartbord/DartbordSection.cs
@@ -8,12 +8,6 @@ public class DartbordSection : MonoBehaviour
     public string MinigameName;
     public int radius;
     public GameObject MinigameLoader;
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.transform.CompareTag("Dart")){
-            ReadyRoomSystem.sceneToLoad = MinigameName;
-        }
-    }
     private void Update() {
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
         foreach (var hitCollider in hitColliders) {
