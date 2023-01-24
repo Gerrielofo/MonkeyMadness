860dc535cbc0c48f0427aeb3f8a63e72c18c4fa6
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2023-01-24 14:36:23 +0100

    dart board gefixed

--- MonkeyMadness/Assets/Gerlof's code/Dartbord/DartbordSection.cs
+++ MonkeyMadness/Assets/Gerlof's code/Dartbord/DartbordSection.cs
@@ -10,6 +10,9 @@ public class DartbordSection : MonoBehaviour
     public GameObject MinigameLoader;
     public void Update() {
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+
         foreach (var hitCollider in hitColliders) {
             if (hitCollider.CompareTag("Dart")) {
                 Debug.Log("Dart Hit");
