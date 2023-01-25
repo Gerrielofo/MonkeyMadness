64f350a5d7c24f9227a027ebc74ecbf7530f79ee
arthur-lava <70568051+arthur-lava@users.noreply.github.com>
2023-01-25 11:32:05 +0100

    Update SpawnBanana.cs

--- MonkeyMadness/Assets/SpawnBanana.cs
+++ MonkeyMadness/Assets/SpawnBanana.cs
@@ -10,11 +10,13 @@ public class SpawnBanana : MonoBehaviour
     private void Start() {
         StartCoroutine(BananaSpawnDelay());
     }
+    [PunRPC]
     IEnumerator BananaSpawnDelay() {
         yield return new WaitForSeconds(3);
         Debug.Log("trying to spawn banana");
         photonView.RPC("SpawnBananaStart", RpcTarget.MasterClient);
     }
+    [PunRPC]
     void SpawnBananaStart() {
         PhotonNetwork.Instantiate("Bananabomb", transform.position, transform.rotation);
         Debug.Log("BananaSpawned");
